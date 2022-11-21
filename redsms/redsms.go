0119eeb746c31b7442aa47213c14aecf257923dd
Sergei Popinevskii <gurza000@gmail.com>
2022-11-21 22:09:11 +0300

    describe client structure

--- redsms/redsms.go
+++ redsms/redsms.go
@@ -1,1 +1,24 @@
 package redsms
+
+import "net/http"
+
+const (
+	defaultBaseURL = "https://cp.redsms.ru/api/"
+)
+
+type Client struct {
+	// httpClient communicates with the API.
+	httpClient *http.Client
+}
+
+// NewClient returns a new RedSMS API client.
+// If a nil httpClient is provided, a new http.Client will be used.
+func NewClient(httpClient *http.Client) *Client {
+	if httpClient == nil {
+		httpClient = &http.Client{}
+	}
+
+	return &Client{
+		httpClient: httpClient,
+	}
+}
