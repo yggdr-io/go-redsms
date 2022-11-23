6b8e0ff951d955144ae730557cf45c1644173145
Sergei Popinevskii <gurza000@gmail.com>
2022-11-23 23:48:35 +0300

    implement Send method

--- redsms/message.go
+++ redsms/message.go
@@ -1,3 +1,47 @@
 package redsms
 
+import "context"
+
 type MessageService service
+
+func (s *MessageService) Send(ctx context.Context, msg *Message) (*SendMessageResponse, *Response, error) {
+	u := "message"
+	req, err := s.client.NewRequest("POST", u, msg)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var respAPI SendMessageResponse
+	resp, err := s.client.Do(ctx, req, &respAPI)
+
+	return &respAPI, resp, nil
+}
+
+type MessageRoute string
+
+const (
+	MessageRouteSMS MessageRoute = "sms"
+)
+
+type Message struct {
+	From  string       `json:"from"`
+	To    string       `json:"to"`
+	Text  string       `json:"text"`
+	Route MessageRoute `json:"route"`
+}
+
+type SendMessageResponse struct {
+	Items   []*SendMessageItem  `json:"items"`
+	Errors  []*SendMessageError `json:"errors"`
+	Success bool                `json:"success"`
+}
+
+type SendMessageItem struct {
+	UUID string `json:"uuid"`
+	To   string `json:"to"`
+}
+
+type SendMessageError struct {
+	To      string `json:"to"`
+	Message string `json:"message"`
+}
