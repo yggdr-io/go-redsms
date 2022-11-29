acfaf04079c0e78613b7139214d6502d30581ca7
Sergei Popinevskii <gurza000@gmail.com>
2022-11-29 17:10:42 +0300

    test Request constructor

--- redsms/redsms_test.go
+++ redsms/redsms_test.go
@@ -1,6 +1,9 @@
 package redsms
 
-import "testing"
+import (
+	"io"
+	"testing"
+)
 
 func TestNewClient(t *testing.T) {
 	c := NewClient(nil)
@@ -12,5 +15,31 @@ func TestNewClient(t *testing.T) {
 	c2 := NewClient(nil)
 	if c.httpClient == c2.httpClient {
 		t.Error("NewClient returned the same http.Client, but they should differ")
+	}
+}
+
+func TestNewRequest(t *testing.T) {
+	c := NewClient(nil)
+
+	inURL, outURL := "foo", defaultBaseURL+"foo"
+	inBody := &struct {
+		Foo string `json:"foo"`
+	}{
+		Foo: "bar",
+	}
+	outBody := `{"foo":"bar"}` + "\n"
+
+	req, _ := c.NewRequest("GET", inURL, inBody)
+
+	if got, want := req.URL.String(), outURL; got != want {
+		t.Errorf("NewRequest(%q) URL is %s, want %s", inURL, got, want)
+	}
+
+	body, _ := io.ReadAll(req.Body)
+	if got, want := string(body), outBody; got != want {
+		t.Errorf("NewRequest() Body is %s, want %s", got, want)
+	}
+	if got, want := req.Header.Get("Content-Type"), "application/json"; got != want {
+		t.Errorf("NewRequest() Content-Type is %s, want %s", got, want)
 	}
 }
