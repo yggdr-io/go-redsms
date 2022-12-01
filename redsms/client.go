842251b6d5b010b46d529474fce70774984fdca0
Sergei Popinevskii <gurza000@gmail.com>
2022-12-01 20:59:59 +0300

    finish implementation of GetInfo method

--- redsms/client.go
+++ redsms/client.go
@@ -4,17 +4,27 @@ import "context"
 
 type ClientService service
 
-func (s *ClientService) GetInfo(ctx context.Context) (*Response, error) {
+func (s *ClientService) GetInfo(ctx context.Context) (*ClientInfo, *Response, error) {
 	u := "client/info"
 	req, err := s.client.NewRequest("GET", u, nil)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	resp, err := s.client.Do(ctx, req, nil)
+	var respAPI struct {
+		Info *ClientInfo `json:"info"`
+	}
+	resp, err := s.client.Do(ctx, req, &respAPI)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	return resp, nil
+	return respAPI.Info, resp, nil
+}
+
+type ClientInfo struct {
+	Login     string   `json:"login"`
+	Balance   float64  `json:"balance"`
+	Active    bool     `json:"active"`
+	Overdraft *float64 `json:"overdraft,omitempty"`
 }
